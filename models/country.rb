43a5dd443f170501a1de55aa3f6dd97530fc35c6
dvioletta <Anita@Pixie.local>
2019-02-09 13:18:29 +0000

    CRUD & Seed done for Country class

---
+++ models/country.rb
@@ -0,0 +1,41 @@
+require_relative('../db/sql_runner')
+
+class Country
+
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(details)
+  @id = details["id"].to_i if details["id"]
+  @name = details["name"]
+  end
+
+  def save #save for adding items to the database
+    sql = "INSERT INTO countries(name)
+    VALUES($1)
+    RETURNING id;"
+    values = [@name]
+    result = SqlRunner.run(sql, values)
+    id = id.to_i
+  end
+
+  def self.find(id) #for finding a country this is required for city method
+    sql= "SELECT * FROM countries
+    WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values).first
+    country = Country.new(result)
+    return country
+  end
+
+  def self.all() #class method to return all countries useful for later updates
+    sql = "SELECT * FROM countries"
+    country_data = SqlRunner.run(sql)
+    country = map.items(country_data)
+    return country
+  end
+
+
+
+
+end
